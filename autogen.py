1975f6641b6ac851bfc5ed1baeb49827273c803d
Larry Lira <larry@expertisesolutions.com.br>
2018-09-27 17:47:34 -0300

    added criteria:
    - MAR
    - PMAX
    - PMIN

--- autogen.py
+++ autogen.py
@@ -8,69 +8,77 @@ from copy import copy
 import itertools
 import argparse
 
-parser = argparse.ArgumentParser(description='TCTautomated.')
-parser.add_argument('--name', '-n', default='tct', help='Test name and where look custom files.')
-parser.add_argument('--dir', '-d', default='tests', help='Destination path to write generated files.')
-parser.add_argument('--efl', '-e', default=None, help='Efl Root Path')
-parser.add_argument('--cls', '-c', default=None, help='Eolian Class')
+parser = argparse.ArgumentParser(description="TCTautomated.")
+parser.add_argument(
+    "--name", "-n", default="tct", help="Test name and where look custom files."
+)
+parser.add_argument(
+    "--dir", "-d", default="tests", help="Destination path to write generated files."
+)
+parser.add_argument("--efl", "-e", default=None, help="Efl Root Path")
+parser.add_argument("--cls", "-c", default=None, help="Eolian Class")
 args = parser.parse_args()
 if not os.path.exists(args.dir):
     os.makedirs(args.dir)
 
 script_path = os.path.dirname(os.path.realpath(__file__))
-root_path = args.efl if args.efl else os.environ['EFL_DIR'] if 'EFL_DIR' in os.environ else '.'
-sys.path.insert(0, os.path.join(root_path, 'src', 'scripts'))
+root_path = (
+    args.efl if args.efl else os.environ["EFL_DIR"] if "EFL_DIR" in os.environ else "."
+)
+sys.path.insert(0, os.path.join(root_path, "src", "scripts"))
 
 try:
     from pyolian import eolian
     from pyolian import pyratemp
     from testgen import suitegen
 except ModuleNotFoundError:
-    print("Efl root path not found, use EFL_DIR environment variable with efl root path in your system\n")
+    print(
+        "Efl root path not found, use EFL_DIR environment variable with efl root path in your system\n"
+    )
     parser.print_help()
     exit()
- 
+
 
 """
 It will find methods and functions with owned return and without other params
 """
 
-@property
-def func_has_enum(self):
-    r = ()
-    for v in self.comp.getter_values or []:
-        if v.type.typedecl and v.type.typedecl.type == eolian.Eolian_Typedecl_Type.ENUM:
-            r = v, *r
-    return r
-
-
 class Template(pyratemp.Template):
-
-    def __init__(self, filename, encoding='utf-8',
-                       loader_class=pyratemp.LoaderFile,
-                       parser_class=pyratemp.Parser,
-                       renderer_class=pyratemp.Renderer,
-                       eval_class=pyratemp.EvalPseudoSandbox):
+    def __init__(
+        self,
+        filename,
+        encoding="utf-8",
+        loader_class=pyratemp.LoaderFile,
+        parser_class=pyratemp.Parser,
+        renderer_class=pyratemp.Renderer,
+        eval_class=pyratemp.EvalPseudoSandbox,
+    ):
 
         global_ctx = {}
-        global_ctx.update({
-            # Template info
-            'date': datetime.datetime.now(),
-            'template_file': os.path.basename(filename),
-        })
+        global_ctx.update(
+            {
+                # Template info
+                "date": datetime.datetime.now(),
+                "template_file": os.path.basename(filename),
+            }
+        )
 
         self.template_filename = filename
-        pyratemp.Template.__init__(self, filename=filename, encoding=encoding,
-                                   data=global_ctx,
-                                   loader_class=loader_class,
-                                   parser_class=parser_class,
-                                   renderer_class=renderer_class,
-                                   eval_class=eval_class)
+        pyratemp.Template.__init__(
+            self,
+            filename=filename,
+            encoding=encoding,
+            data=global_ctx,
+            loader_class=loader_class,
+            parser_class=parser_class,
+            renderer_class=renderer_class,
+            eval_class=eval_class,
+        )
 
     def render(self, suite, verbose=True):
         # Build the context for the template
         ctx = {}
-        ctx['suite'] = suite
+        ctx["suite"] = suite
         # render with the augmented context
         output = self(**ctx)
 
@@ -80,46 +88,54 @@ class Template(pyratemp.Template):
                 f.write(output)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     def _load_class(eocls):
         if eocls and eocls.type == eocls.type.REGULAR and not eocls.name in blacklist:
-            name = '_'.join([args.name, eocls.name.replace(".", "_")])
-            suite = suitegen.SuiteGen(args.name, name, "{}.cs".format(os.path.join(args.dir, name)), args.name, 'tct_suite.template')
+            name = "_".join([args.name, eocls.name.replace(".", "_")])
+            suite = suitegen.SuiteGen(
+                args.name,
+                name,
+                "{}.cs".format(os.path.join(args.dir, name)),
+                args.name,
+                "tct_suite.template",
+            )
             suite.loadObj(eocls)
             t = Template(suite.template)
-            try:
-                t.render(suite)
-            except:
-                print("ERROR RENDERING - Cannot create file: {}".format(suite.filename))
+            t.render(suite)
+
+    #            try:
+    #                t.render(suite)
+    #            except:
+    #                print("ERROR RENDERING - Cannot create file: {}".format(suite.filename))
 
     # Use .eo files from the source tree (not the installed ones)
-    SCAN_FOLDER = os.path.join(root_path, 'src', 'lib')
+    SCAN_FOLDER = os.path.join(root_path, "src", "lib")
 
     # create main eolian state
     eolian_db = eolian.Eolian_State()
     if not isinstance(eolian_db, eolian.Eolian_State):
-        raise(RuntimeError('Eolian, failed to create Eolian state'))
+        raise (RuntimeError("Eolian, failed to create Eolian state"))
 
     # eolian source tree scan
     if not eolian_db.directory_add(SCAN_FOLDER):
-        raise(RuntimeError('Eolian, failed to scan source directory'))
+        raise (RuntimeError("Eolian, failed to scan source directory"))
 
     # Parse all known eo files
     if not eolian_db.all_eot_files_parse():
-        raise(RuntimeError('Eolian, failed to parse all EOT files'))
+        raise (RuntimeError("Eolian, failed to parse all EOT files"))
 
     if not eolian_db.all_eo_files_parse():
-        raise(RuntimeError('Eolian, failed to parse all EO files'))
+        raise (RuntimeError("Eolian, failed to parse all EO files"))
 
     # cleanup the database on exit
     import atexit
+
     def cleanup_db():
         global eolian_db
         del eolian_db
-    atexit.register(cleanup_db)
 
-    suitegen.FuncItem.has_enum = func_has_enum
+    atexit.register(cleanup_db)
 
     blacklist = []
     if args.cls:
@@ -129,5 +145,4 @@ if __name__ == '__main__':
         _load_class(eocls)
     else:
         for eocls in eolian_db.classes:
-            _load_class(eocls);
-
+            _load_class(eocls)
